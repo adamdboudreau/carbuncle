3d754870e3661cb53fb75a9ea49ebf9ffdaba3a0
Adam Boudreau <aboudreau@lush.io>
2013-11-21 13:14:33 -0500

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carbuncle::Application.config.secret_key_base = '9a231613aea039a3b659617f5e918a251e85d5baa33ce76b27116c37e23c44376ad3e322ae207a220ef181c2cf239c47aadc0a54a02c0358c74fa81274bff387'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Carbuncle::Application.config.session_store :cookie_store, key: '_carbuncle_session'
