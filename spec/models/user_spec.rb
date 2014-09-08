ab12cf7c67ec1e549c1177bda1bc3a02a873b5be
Adam Boudreau <adamdboudreau@gmail.com>
2014-09-08 16:27:08 -0400

    Comment out devise tests.  Add tests for User.similar_emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,31 @@
 require 'spec_helper'
-#require './app/models/user'
 
 describe User do
-  it { should validate_presence_of :email }
-  # it { should validate_format_of :email, with: /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i }
-
-  it { should_not allow_value('a@b').for(:email) }
-
-  it { should allow_value('a@b.c').for(:email) }
   describe "#valid?" do
     it "valid factory" do
       FactoryGirl.build(:user).valid?.should be_true
     end
   end
+
+  describe ".similar_emails" do
+    let(:user) { FactoryGirl.create(:user) }
+    it "should return all users when given nil" do
+      User.similar_emails(nil).should == [user]
+    end
+
+    it "should return users with the partial email given" do
+      User.similar_emails("john").should == [user]
+    end
+
+    it "should not return users that don't match the partial given" do
+      User.similar_emails("somebodygonnagetahurtrealbad").should be_empty
+    end
+  end
+
+  # Commenting out the tests that are already covered by devise tests
+  # it { should validate_presence_of :email }
+  # %w{invalid_email_format 123 $$$ () ☃ bla@bla.}.each do |email|
+  #   it { should_not allow_value(email).for(:email) }
+  # end
+  # it { should allow_value('a@b.c').for(:email) }
 end
