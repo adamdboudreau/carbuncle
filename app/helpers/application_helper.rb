b42f808aa028c4ddf4d3fa18796a28dd9dc69582
Adam Boudreau <adamdboudreau@gmail.com>
2014-09-11 20:49:27 -0400

    Remove some useless scripts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    css_class = (column == @sort) ? "current &crarr; #{(@asc == 1) ? 'asc' : 'desc'}" : nil
+    css_class = (column == @sort) ? "current #{(@asc == 1) ? 'asc' : 'desc'}" : nil
     direction = (column == @sort && @asc == 1) ? 0 : 1
     #link_to title, {:sort => column, :direction => direction}, {:class => css_class}
     link_to title, params.merge(sort: column, asc: direction, page: nil), {:class => css_class}
